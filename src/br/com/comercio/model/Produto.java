1669577f0279bf2baa2543c14fcd6af22c22a755
vfcarneiro <vfcarneiro@sga.pucminas.br>
2025-07-02 17:02:57 -0300

    feat: adicionar metódo do estoque

---
+++ src/br/com/comercio/model/Produto.java
@@ -0,0 +1,52 @@
+package br.com.comercio.model;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private int quantidadeEstoque;
+    private double precoVenda;
+
+    public Produto(int codigo, String nome, int quantidadeEstoque, double precoVenda) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.quantidadeEstoque = (int) precoVenda;
+        this.precoVenda = precoVenda;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    @Override
+    public String toString() {
+        return "Informações do produto: codigo=" + codigo + ", nome=" + nome + ", estoque=" + quantidadeEstoque + ", preco=R$" + precoVenda + "]";
+    }
+}
